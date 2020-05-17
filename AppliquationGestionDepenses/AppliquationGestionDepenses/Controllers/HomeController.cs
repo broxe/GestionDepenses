afc4c84c6d104c495daed3c844a5aef866d76894
BOUSSIF.Ayoub <centrino20111@hotmail.fr>
2020-05-17 02:38:21 +0200

    Ajout : - Delete / Edit - Configuration & Statistique

--- AppliquationGestionDepenses/AppliquationGestionDepenses/Controllers/HomeController.cs
+++ AppliquationGestionDepenses/AppliquationGestionDepenses/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace AppliquationGestionDepenses.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public ActionResult GetData()
         {
             using (DatabaseDepensesEntities dataDepense = new DatabaseDepensesEntities())
@@ -24,9 +24,18 @@ namespace AppliquationGestionDepenses.Controllers
             }
         }
 
+        [HttpGet]
         public ActionResult AddOrEdit(int id =0)
         {
-            return View(new TDepenses());
+            if (id == 0)
+                return View(new TDepenses());
+            else
+            {
+                using (DatabaseDepensesEntities dataDepense = new DatabaseDepensesEntities())
+                {
+                    return View(dataDepense.TDepenses.Where(x => x.IdDepense == id).FirstOrDefault<TDepenses>());
+                }
+            }
         }
 
         [HttpPost]
@@ -34,9 +43,23 @@ namespace AppliquationGestionDepenses.Controllers
         {
             using (DatabaseDepensesEntities dataDepense = new DatabaseDepensesEntities())
             {
+                depense.IdDepense = dataDepense.TDepenses.Max(x => x.IdDepense) +1;
                 dataDepense.TDepenses.Add(depense);
                 dataDepense.SaveChanges();
                 return Json(new { succes = true, message = "Saved Succefully" });
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DatabaseDepensesEntities dataDepense = new DatabaseDepensesEntities())
+            {
+                TDepenses tmps = dataDepense.TDepenses.Where(x => x.IdDepense == id).FirstOrDefault<TDepenses>();
+                dataDepense.TDepenses.Remove(tmps);
+                dataDepense.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
             }
         }
 
--- AppliquationGestionDepenses/AppliquationGestionDepenses/Controllers/StatistiqueController.cs
+++ AppliquationGestionDepenses/AppliquationGestionDepenses/Controllers/StatistiqueController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppliquationGestionDepenses.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +14,58 @@ namespace AppliquationGestionDepenses.Controllers
         {
             return View();
         }
+
+        //  Obtenir statistique par type
+        public ActionResult GetStat ()
+        {
+            Dictionary<string, int> dictData = new Dictionary<string, int>();
+
+            using (DatabaseDepensesEntities dataDepense = new DatabaseDepensesEntities())
+            {
+                List<TDepenses> listDepenses = dataDepense.TDepenses.ToList<TDepenses>();                       //  Obtention de tout les données 
+
+                foreach (var it in listDepenses)
+                {
+                    if (dictData.ContainsKey(it.typeDepense.Trim()))
+                    {
+                        dictData[it.typeDepense.Trim()] = dictData[it.typeDepense.Trim()] + 1;
+                    }
+                    else
+                    {
+                        dictData.Add(it.typeDepense.Trim(), 1);
+                    }
+                }
+
+                return Json(new { data = dictData }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //  Obtenir statistique prix par type
+        public ActionResult GestStatParPrix()
+        {
+            Dictionary<string, double> dictData = new Dictionary<string, double>();
+
+            using (DatabaseDepensesEntities dataDepense = new DatabaseDepensesEntities())
+            {
+                List<TDepenses> listDepenses = dataDepense.TDepenses.ToList<TDepenses>();                       //  Obtention de tout les données 
+
+                foreach (var it in listDepenses)
+                {
+                    if (dictData.ContainsKey(it.typeDepense.Trim()))
+                    {
+                        dictData[it.typeDepense.Trim()] = dictData[it.typeDepense.Trim()] + it.prixDepense;
+                    }
+                    else
+                    {
+                        dictData.Add(it.typeDepense.Trim(), it.prixDepense);
+                    }
+                }
+
+                return Json(new { data = dictData }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //  Sta
+
     }
 }
